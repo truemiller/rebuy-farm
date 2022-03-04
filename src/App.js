469d541503fdc682f8cc0f108219b1a5467e809f
Josh <31908788+truemiller@users.noreply.github.com>
2022-03-04 18:59:59 +0000

    list format

--- src/App.js
+++ src/App.js
@@ -36,7 +36,8 @@ function App() {
                         <section className="row mb-3">
                             <div className="col-md-3"></div>
                             <div className="col-md-6">
-                                <input type="text" className="form-control mb-3 shadow-lg" placeholder={"Search"} onChange={event=>setQuery(event.target.value)}/></div>
+                                <input type="text" className="form-control mb-3 shadow-lg" placeholder={"Search"}
+                                       onChange={event => setQuery(event.target.value)}/></div>
                         </section>
                         <section>
                             <VaultTable accounts={accounts} chainId={chainId} query={query}/>
@@ -44,7 +45,8 @@ function App() {
                     </main>
                     <footer className="footer bg-gradient mt-auto">
                         <div className="container">
-                            <span className="text-white-50">&copy; <a href="//truemiller.com" className={"text-decoration-none"}>True Miller</a> 2021. All rights reserved. This dApp (decentralized application) is in beta, use at your own risk. Smart contracts are experimental. Contact us <a
+                            <span className="text-white-50">&copy; <a href="//truemiller.com"
+                                                                      className={"text-decoration-none"}>True Miller</a> 2021. All rights reserved. This dApp (decentralized application) is in beta, use at your own risk. Smart contracts are experimental. Contact us <a
                                 href="//t.me/truemiller1">here</a>.</span>
                         </div>
                     </footer>
@@ -52,7 +54,7 @@ function App() {
             </Fragment>
             :
             <>
-                    <Navbar/>
+                <Navbar/>
                 <header id="hero" className="text-center">
                     <h1 className={"fw-bolder text-light display-1"}>Rebuy Farm</h1>
                     <p className={"lead text-white-50"}>The cross chain yield optimizer</p>
@@ -88,7 +90,8 @@ function Navbar(props) {
                 <a href="//twitter.com/rebuyfarm" className="nav-link text-white">Twitter</a>
             </div>
             <div className={"d-flex"}>
-                <a href={""} className="nav-link text-white">{accounts && chainIds[chainId]? `Connected to ${chainIds[chainId]}` : "Not connected"}</a>
+                <a href={""}
+                   className="nav-link text-white">{accounts && chainIds[chainId] ? `Connected to ${chainIds[chainId]}` : "Not connected"}</a>
             </div>
         </div>
     </nav>
@@ -100,8 +103,8 @@ function VaultTable(props) {
     const query = props.query
     const isConnected = accounts.length > 0
     const vaultArray = Object.keys(vaults).map(vaultKey => vaults[vaultKey])
-        .filter(vault=>vault.chain.chainId === chainId)
-        .filter(vault=>{
+        .filter(vault => vault.chain.chainId === chainId)
+        .filter(vault => {
             const lowerCaseQuery = query.toLowerCase()
             const platformName = vault.platform.name.toLowerCase()
             const strategyName = vault.strategy.name.toLowerCase()
@@ -116,19 +119,19 @@ function VaultTable(props) {
     return <div className={"row"}>
         {
             vaultArray.length > 0 ?
-            vaultArray.map(vault => {
-                const farm = vault.farm
-                const vaultChainId = vault.chain.chainId
-                return vaultChainId === chainId ? (
-                    <div className={"col-xl-3 col-lg-4 col-md-6 mb-3"} key={vault.address}>
-                        <VaultTableRow vault={vault} farm={farm} accounts={accounts}/>
-                    </div>
-                ) : null
-            })
+                vaultArray.map(vault => {
+                    const farm = vault.farm
+                    const vaultChainId = vault.chain.chainId
+                    return vaultChainId === chainId ? (
+                        <div className={"col-xl-12 mb-3"} key={vault.address}>
+                            <VaultTableRow vault={vault} farm={farm} accounts={accounts}/>
+                        </div>
+                    ) : null
+                })
                 :
                 <>
                     <div className="alert alert-danger" role="alert">
-                        There aren't any farms! Try using <a href="#" onClick={()=>
+                        There aren't any farms! Try using <a href="#" onClick={() =>
                         window?.ethereum?.request({
                             method: 'wallet_switchEthereumChain',
                             params: [{chainId: '0xa86a'}], // chainId must be in hexadecimal numbers})
@@ -148,9 +151,11 @@ function VaultTableRow(props) {
     let vaultKey = props.vaultKey
     let [accounts, setAccounts] = useState(props.accounts)
 
+    let [lpUsd, setLpUsd] = useState(null)
     let [tvl, setTvl] = useState(null)
     let [tvlUSD, setTvlUSD] = useState(null)
     let [wallet, setWallet] = useState(null)
+    let [walletUsd, setWalletUsd] = useState(null)
     let [deposited, setDeposited] = useState(null)
     let [currentStake, setCurrentStake] = useState(null)
     let [apr, setApr] = useState(null)
@@ -158,124 +163,191 @@ function VaultTableRow(props) {
     let [currentStakeUSD, setCurrentStakeUSD] = useState(null)
     let [rewards, setRewards] = useState(null)
 
+    let [expanded, setExpanded] = useState(false)
     let [approved, setApproved] = useState(false)
 
 
     useEffect(async () => {
-        const isApproved =  await vault.isApprovedPromise()
-        setApproved(isApproved)
 
-        const apr = await farm?.aprPromise()
-        setApr(apr)
-        const apy = (((1 + (apr / 100 / 365)) ** 365) - 1) * 100
-        setApy(apy)
-        const tvl = await vault.tvlPromise().then(r => web3.utils.fromWei(r.toString()))
-        setTvl(tvl)
-        let lpUsdPerToken = await lp.usdPerToken()
+        // const apr = await farm?.aprPromise()
+        // const tvl = await vault.tvlPromise().then(r => web3.utils.fromWei(r.toString()))
+        // let lpUsdPerToken = await lp.usdPerToken()
+        //
+        // const walletBalance = await farm.token.balanceOfPromise().then(r => web3.utils.fromWei(r.toString()))
+        // const initialStake = await vault.depositedPromise().then(r => web3.utils.fromWei(r.toString()))
+        // const currentStake = await vault.getPricePerFullSharePromise().then(r => web3.utils.fromWei(r.toString())) * initialStake
 
-        const rewards = await vault.strategy.rewardsAvailablePromise().then(r => r)
-        const tvlUSD = parseFloat(lpUsdPerToken) * parseFloat(tvl)
-        setTvlUSD(tvlUSD)
-        const walletBalance = await farm.token.balanceOfPromise().then(r => web3.utils.fromWei(r.toString()))
-        setWallet(walletBalance)
-        const initialStake = await vault.depositedPromise().then(r => web3.utils.fromWei(r.toString()))
-        setDeposited(initialStake)
+        const [apr,tvl,lpUsdPerToken,walletBalance,initialStake] = await Promise.all([
+            farm?.aprPromise(), //apr
+            vault.tvlPromise().then(r => web3.utils.fromWei(r.toString())), //tvl
+            lp.usdPerToken(), //usdpertoken
+            farm.token.balanceOfPromise().then(r => web3.utils.fromWei(r.toString())),
+            vault.depositedPromise().then(r => web3.utils.fromWei(r.toString()))
+        ])
+
         const currentStake = await vault.getPricePerFullSharePromise().then(r => web3.utils.fromWei(r.toString())) * initialStake
-        setCurrentStake(currentStake)
-        const currentStakedUSD = currentStake * lpUsdPerToken
-        setCurrentStakeUSD(currentStakedUSD)
-        setRewards(rewards)
 
+        const apy = (((1 + (apr / 100 / 365)) ** 365) - 1) * 100
+        const tvlUSD = parseFloat(lpUsdPerToken) * parseFloat(tvl)
+        const currentStakedUSD = currentStake * lpUsdPerToken
+
+        setApr(apr)
+        setApy(apy)
+        setTvl(tvl)
+        setLpUsd(lpUsdPerToken)
+        setTvlUSD(tvlUSD)
+        setWallet(walletBalance)
+        setDeposited(parseFloat(initialStake))
+        setCurrentStake(currentStake)
+        setCurrentStakeUSD(currentStakedUSD)
 
     }, [])
 
-    return <div key={vaultKey} className={"card border  shadow-lg"}>
-        <div className={"card-body "}>
-            <div className={"fw-bolder text-center"}>
-                <h2 className={"fw-bolder"}><a className={"text-decoration-none"} href={farm.exchange.url}>{farm.exchange.name}</a></h2>
-                {farm.name}
-                <div className={"mb-3"}>
-                    <img loading={"lazy"} src={farm.token.token0.image}
+    useEffect(async () => {
+        if (expanded) {
+            const isApproved = await vault.isApprovedPromise()
+            setApproved(isApproved)
+            const rewards = await vault.strategy.rewardsAvailablePromise().then(r => r)
+            setRewards(rewards)
+            setWalletUsd(wallet * lpUsd)
+        }
+    }, [expanded])
+
+    return <div key={vaultKey} className={"card border shadow-lg"}>
+        <div className={"card-body d-flex justify-content-between"}>
+            <div className={"text-center d-flex mt-auto mb-auto"}>
+                <img loading={"lazy"} src={farm.token.token0.image}
+                     alt={farm.platform.name + " logo"}
+                     title={farm.platform.name} height={24} width={24}/>
+                {!farm.token.isSingle ?
+                    <img loading={"lazy"}
+                         src={farm.token.token1.image}
                          alt={farm.platform.name + " logo"}
-                         title={farm.platform.name} height={24} width={24}/>
-                    {!farm.token.isSingle ?
-                        <img loading={"lazy"}
-                             src={farm.token.token1.image}
-                             alt={farm.platform.name + " logo"}
-                             title={farm.platform.name}
-                             height={24} width={24}/> : null }
-                </div>
-                {vault.note ? <div className={"alert alert-info"}>{vault.note}</div> : null }
+                         title={farm.platform.name}
+                         height={24} width={24}/> : null}
             </div>
-            <table className={"table table-sm table-bordered"}>
-                <tbody>
-                <tr>
-                    <th>APR</th>
-                    <td>{apr != null ? parseFloat(apr).toFixed(4) : <Skeleton/>}</td>
-                </tr>
-                <tr>
-                    <th>APY</th>
-                    <td>{apy != null ? parseFloat(apy).toFixed(4) : <Skeleton/>}</td>
-                </tr>
-
-                <tr>
-                    <th>TVL</th>
-                    <td>{tvl != null ? parseFloat(tvl).toFixed(4) : <Skeleton/>}</td>
-                </tr>
-                <tr>
-                    <th>TVL ($)</th>
-                    <td>{tvlUSD != null ? parseFloat(tvlUSD).toFixed(0) : <Skeleton/>}</td>
-                </tr>
-                <tr>
-                    <th>Wallet Balance</th>
-                    <td>{wallet != null ? parseFloat(wallet).toPrecision(4) : <Skeleton/>}</td>
-                </tr>
-                {/*<tr>*/}
-                {/*    <th>Deposited</th>*/}
-                {/*    <td>{deposited ? parseFloat(deposited).toFixed(4) :*/}
-                {/*        <Skeleton/>}</td>*/}
-                {/*</tr>*/}
-                <tr>
-
-                    <th>Current stake</th>
-                    <td>{currentStake != null ? parseFloat(currentStake).toFixed(4) :
-                        <Skeleton/>}</td>
-                </tr>
-                <tr>
-                    <th>Current stake ($)</th>
-                    <td>{currentStakeUSD != null ? (parseFloat(currentStakeUSD).toFixed(0)) : <Skeleton/>}</td>
-                </tr>
-                <tr>
-                    <th>Awaiting compound</th>
-                    <td>{rewards != null ? (parseFloat(rewards).toPrecision(4)) : <Skeleton/>}</td>
-                </tr>
-                </tbody>
-            </table>
-            <div className="btn-group w-100" role="group" aria-label="...">
-                <button className="btn btn-sm btn-success"
-                        onClick={async () => vault.harvestPromise()}>
-                    Compound
-                </button>
-                <button className="btn btn-sm btn-primary"
-                        onClick={vault.withdrawAllPromise}>
-                    Withdraw
-                </button>
-
-                { // Approval switch
-                    approved ? (
-                        <button className="btn btn-sm btn-dark"
-                                onClick={vault.depositAllPromise}>
-                            Deposit
-                        </button>
-                    ) : (
-                        <button className="btn btn-sm btn-danger"
-                                onClick={vault.approvePromise}>
-                            Approve
-                        </button>
-                    )
-                }
+            <div className="d-flex flex-column align-self-start">
+                <strong>{farm.name}</strong>
+                <strong className={"fw-bolder d-inline-block"}><a className={"text-decoration-none text-truncate"}
+                                                                  href={farm.exchange.url}>{farm.exchange.name}</a></strong>
+            </div>
+            <div className="d-flex flex-column text-center">
+                {wallet ? wallet : <Skeleton/>}
+                <strong>Balance</strong>
+            </div>
+            <div className="d-flex flex-column text-center">
+                {currentStake != null ? currentStake.toFixed(4) : <Skeleton/>}
+                <strong>Deposited</strong>
+            </div>
+            <div className="d-flex flex-column text-center">
+                {apy ? apy.toFixed(2) : <Skeleton/>}%
+                <strong>APY</strong>
+            </div>
+            <div className="d-flex flex-column text-center">
+                {apy ? (apy / 365).toFixed(2) : <Skeleton/>}%
+                <strong>Daily</strong>
+            </div>
+            <div className="d-flex flex-column text-center">
+                ${tvlUSD != null ? tvlUSD.toFixed(2).toLocaleString() : <Skeleton/>}
+                <strong>TVL</strong>
+            </div>
+            <div className="d-flex flex-column text-center align-self-end">
+                <a href="#" className="btn btn-dark btn-lg" onClick={() => setExpanded(!expanded)}>More</a>
             </div>
         </div>
+        {
+            expanded ?
+                <div className="card-footer">
+                    {vault.note ? <div className={"alert alert-info"}>{vault.note}</div> : null}
+                    <div className="row">
+                        <div className="col-md-4 d-flex flex-column">
+                            <h3>Wallet</h3>
+                            <span className={"mb-3"}>
+                            <strong>Balance</strong>: {wallet} (${wallet})
+                            </span>
+                            { // Approval switch
+                                approved ? (
+                                    <button className="btn btn-lg btn-dark"
+                                            onClick={vault.depositAllPromise}>
+                                        Deposit
+                                    </button>
+                                ) : (
+                                    <button className="btn btn-lg btn-danger"
+                                            onClick={vault.approvePromise}>
+                                        Approve
+                                    </button>
+                                )
+                            }
+                        </div>
+                        <div className="col-md-4 d-flex flex-column">
+                            <h3>Vault</h3>
+                            <span className={"mb-3"}>
+                            <strong>Deposited</strong>: {currentStake ? currentStake.toFixed(4) : 0} (${currentStakeUSD ? currentStakeUSD.toFixed(4): 0})
+                            </span>
+                            <button className="btn btn-lg btn-dark"
+                                    onClick={vault.withdrawAllPromise}>
+                                Withdraw
+                            </button>
+                        </div>
+                        <div className="col-md-4 d-flex flex-column">
+                            <h3>Pending</h3>
+                            <span className={"mb-3"}>
+                                <strong>Rewards</strong>: {0}
+                            </span>
+                            <button className="btn btn-lg btn-success"
+                                    onClick={async () => vault.harvestPromise()}>
+                                Compound
+                            </button>
+
+                        </div>
+                    </div>
+                    <hr/>
+                    <table className={"table table-sm table-bordered"}>
+                        <tbody>
+                        <tr>
+                            <th>APR</th>
+                            <td>{apr != null ? parseFloat(apr).toFixed(4) : <Skeleton/>}</td>
+                        </tr>
+                        <tr>
+                            <th>APY</th>
+                            <td>{apy != null ? parseFloat(apy).toFixed(4) : <Skeleton/>}</td>
+                        </tr>
+
+                        <tr>
+                            <th>TVL</th>
+                            <td>{tvl != null ? parseFloat(tvl).toFixed(4) : <Skeleton/>}</td>
+                        </tr>
+                        <tr>
+                            <th>TVL ($)</th>
+                            <td>{tvlUSD != null ? parseFloat(tvlUSD).toFixed(0) : <Skeleton/>}</td>
+                        </tr>
+                        <tr>
+                            <th>Wallet Balance</th>
+                            <td>{wallet != null ? parseFloat(wallet).toPrecision(4) : <Skeleton/>}</td>
+                        </tr>
+                        {/*<tr>*/}
+                        {/*    <th>Deposited</th>*/}
+                        {/*    <td>{deposited ? parseFloat(deposited).toFixed(4) :*/}
+                        {/*        <Skeleton/>}</td>*/}
+                        {/*</tr>*/}
+                        <tr>
+
+                            <th>Current stake</th>
+                            <td>{currentStake != null ? parseFloat(currentStake).toFixed(4) :
+                                <Skeleton/>}</td>
+                        </tr>
+                        <tr>
+                            <th>Current stake ($)</th>
+                            <td>{currentStakeUSD != null ? (parseFloat(currentStakeUSD).toFixed(0)) : <Skeleton/>}</td>
+                        </tr>
+                        <tr>
+                            <th>Awaiting compound</th>
+                            <td>{rewards != null ? (parseFloat(rewards).toPrecision(4)) : <Skeleton/>}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div> : null
+        }
     </div>
 }
 
